34e025c76769909c44f0219c9faff78c3cb24571
Armin Eslami <armin.eslami@gmail.com>
2017-11-23 01:00:49 -0500

    generate components: navbar, home and shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
