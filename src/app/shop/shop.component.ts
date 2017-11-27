221fc7ffe8f1b04fcb7e10ab8254b7304d1be4c3
Armin Eslami <armin.eslami@gmail.com>
2017-11-27 01:20:29 -0500

    feat: get angular cli to process scss

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -6,7 +6,7 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: "app-shop",
   templateUrl: "./shop.component.html",
-  styleUrls: ["./shop.component.css"]
+  styleUrls: ["./shop.component.scss"]
 })
 export class ShopComponent implements OnInit {
   drinkList;
